6ed533bc05672b2f8d92011ba13dcb26ac2d52ce
Klaad <s1128540@student.windesheim.nl>
2023-10-11 10:16:00 +0200

    Initial testproject setup
    Setup testproject with xUnit Template

---
+++ Showcase/Showcase.Test/ControllerTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Showcase.Test.ControllerTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        public CustomWebApplicationFactory()
+        {
+            
+        }
+    }
+}
